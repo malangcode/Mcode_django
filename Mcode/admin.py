bfd129c4b55f248d18e0c2663b16392fb5a00320
smartcoderrahis <rahissheikh200@gmail.com>
2025-07-07 21:00:04 +0545

    added hero in admin

--- Mcode/admin.py
+++ Mcode/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Hero
+
+# Register your models here.
+
+
+
+@admin.register(Hero)
+class HeroAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'image')
+
 
 # Register your models here.
