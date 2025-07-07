0ce821b9c59132e5fa4ddaa40f11178046368b43
smartcoderrahis <rahissheikh200@gmail.com>
2025-07-07 20:54:50 +0545

    updated settings

--- Mcode/models.py
+++ Mcode/models.py
@@ -8,4 +8,3 @@ class Hero(models.Model):
 
     def __str__(self):
         return self.name
-
--- McodeProject/settings.py
+++ McodeProject/settings.py
@@ -27,7 +27,19 @@ SECRET_KEY = 'django-insecure-7lm!pay9xbu_q76fdpjge(5%y*qawu-9x^^e!f@%clte5_znz_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['malangcode.pythonanywhere.com',]
+ALLOWED_HOSTS = ['malangcode.pythonanywhere.com', 'localhost', '127.0.0.1']
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",   # React/Next.js dev
+    "https://127.0.0.1:3000",  # React/Next.js dev
+    "https://malangcode.pythonanywhere.com",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "https://127.0.0.1:3000",
+    "https://malangcode.pythonanywhere.com",
+]
 
 
 # Application definition
@@ -45,6 +57,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
