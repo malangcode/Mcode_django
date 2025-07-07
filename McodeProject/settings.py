0a423272038e3606bc8f655ef88f4b4603fefd43
smartcoderrahis <rahissheikh200@gmail.com>
2025-07-07 21:19:59 +0545

    added

--- McodeProject/settings.py
+++ McodeProject/settings.py
@@ -34,6 +34,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://127.0.0.1:3000",  # React/Next.js dev
     "https://malangcode.pythonanywhere.com",
     "http://127.0.0.1:5500",
+    "http://localhost:5500",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
@@ -41,6 +42,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://127.0.0.1:3000",
     "https://malangcode.pythonanywhere.com",
     "http://127.0.0.1:5500",
+    "http://localhost:5500",
 ]
 
 
