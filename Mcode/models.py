ceb585e002d8d392620a4ac1e52b5437344d609a
smartcoderrahis <rahissheikh200@gmail.com>
2025-07-09 19:15:27 +0545

    changed field type for number

--- Mcode/models.py
+++ Mcode/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class HeroStats(models.Model):
     label = models.CharField(max_length=200)
-    number = models.IntegerField()
+    number = models.CharField(max_length=200)
     icon = models.CharField(max_length=200)
 
     def __str__(self):
