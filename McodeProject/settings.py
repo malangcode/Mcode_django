23f93ab80e77e20bdd0d9009fc183a563e56c855
rahis <rahissheikh200@gmail.com>
2025-07-12 12:50:25 +0545

    added templates

--- McodeProject/settings.py
+++ McodeProject/settings.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = 'McodeProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
